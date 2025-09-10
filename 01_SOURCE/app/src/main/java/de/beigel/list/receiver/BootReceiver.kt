0caf69436765cb6be241032c5cb9ae181e37755e
beigel <beigel.dev@gmail.com>
2025-09-10 13:04:55 +0200

    Revert "upload"
    This reverts commit 76deda11fe72be6a3ae6a4bb1c08444781597fb9.

--- 01_SOURCE/app/src/main/java/de/beigel/list/receiver/BootReceiver.kt
+++ 01_SOURCE/app/src/main/java/de/beigel/list/receiver/BootReceiver.kt
@@ -3,13 +3,7 @@ package de.beigel.list.receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import de.beigel.list.service.MidnightResetWorker
-import de.beigel.list.service.NotificationWorker
 import de.beigel.list.settings.SettingsManager
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.launch
 
 class BootReceiver : BroadcastReceiver() {
 
@@ -21,16 +15,13 @@ class BootReceiver : BroadcastReceiver() {
             MidnightResetWorker.scheduleInitialWork(context)
 
             // Reschedule notifications if enabled
-            CoroutineScope(Dispatchers.IO).launch {
-                val settingsManager = SettingsManager(context)
-                val notificationsEnabled = settingsManager.notificationsEnabled.first()
-
-                if (notificationsEnabled) {
-                    val hour = settingsManager.notificationHour.first()
-                    val minute = settingsManager.notificationMinute.first()
-
-                    NotificationWorker.scheduleDaily(context, hour, minute)
-                }
+            val settingsManager = SettingsManager(context)
+            if (settingsManager.notificationsEnabled) {
+                NotificationWorker.scheduleDaily(
+                    context,
+                    settingsManager.notificationHour,
+                    settingsManager.notificationMinute
+                )
             }
         }
     }
