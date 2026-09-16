cc01a13575f38cd083e2c851246455a63ce68afe
beigel <44787264+B31G3L@users.noreply.github.com>
2026-09-16 10:49:35 +0200

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.beigel.list2share"
         minSdk = 29
         targetSdk = 36
-        versionCode = 11
-        versionName = "0.2.3"
+        versionCode = 12
+        versionName = "0.2.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
